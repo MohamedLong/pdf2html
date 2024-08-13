c2d3a8eaff8afe52ce87dc37636ec7c8b7c7b97f
Mohamed Ali <63055862+MohamedLong@users.noreply.github.com>
2024-08-13 17:45:57 +0400

    Delete main directory

--- main/main.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def convert_pdf_to_html(pdf_path, output_folder):
-  command = ['pdf2htmlEX', pdf_path, output_folder]
-  subprocess.run(command, check=True)
-pdf_path = "BankMuscatReport.pdf"
-output_folder = "output_html"
-convert_pdf_to_html(pdf_path, output_folder)
